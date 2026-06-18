27cd4037d2de570cfee8d4e47103053ebc23db43
intharshrungta-stack <intharshrungta@gmail.com>
2026-06-18 22:32:35 +0530

    Add files via upload

---
+++ Financial_Loan_SQL_file.sql
@@ -0,0 +1,10 @@
+SELECT
+    loan_status,
+    SUM(total_payment) AS MTD_Total_Amount_Received,
+    SUM(loan_amount) AS MTD_Total_Funded_Amount
+FROM
+    financial_loan
+WHERE
+    MONTH(issue_date) = 12
+GROUP BY
+    loan_status;
